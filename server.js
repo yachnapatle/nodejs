c0bf0ed6c25bf46a8fe1a1b8a8f918fd622e1600
yachnapatle <0506cs211067@gmail.com>
2024-08-10 09:15:45 +0530

    nodejs

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 let express=require('express')
 let app=express();
-app.use((req,res)=>
-    {
-    res.send('mai to nhi jane dera');
-    })
+// app.use((req,res)=>
+//     {
+//     res.send('mai to nhi jane dera');
+//     })
 app.get('/',(req,res)=>
 {
 
@@ -16,6 +16,8 @@ app.get('/home',(req,res)=>
     res.send('hello this is you server at home');
     
     })
+
+    
 app.listen(4000,()=>
 {
     console.log('server running on port number 4000');
